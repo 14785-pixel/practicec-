0e94d238d9d93c490a5e0f9fe9797d02f750037f
14785-pixel <54209983+14785-pixel@users.noreply.github.com>
2020-06-21 02:28:36 +0530

    Update listinsert.py

--- listinsert.py
+++ listinsert.py
@@ -1,12 +1,14 @@
 #Inserts in sorted order in a sorted list
 def insert(x,arr):
-    if len==0:
+    if len(arr)==0:
         arr.append(x)
         return arr
     if arr[-1]<=x:
          arr.append(x)
          return arr     
     return insert(x,arr[:-1])+[arr[-1]]
+
+
 
 
     
